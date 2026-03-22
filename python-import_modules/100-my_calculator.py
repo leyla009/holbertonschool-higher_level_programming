3a2f4a68c8d7c1fa12ecb9b6bfe9b98c406c2124
Leyla <Leyla.khaspolad@gmail.com>
2026-03-22 16:31:55 +0000

    Fix indendation errors

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
 
     ops = {
         "+": add,
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
     result = ops[operator](a, b)
     print("{} {} {} = {}".format(a, operator, b, result))
