fcc126d3ea71bc504f0a8306e8b0c7b4037ada02
Leyla <Leyla.khaspolad@gmail.com>
2026-03-22 18:37:45 +0000

    Add task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+
+    res_1 = a[0] + b[0]
+    res_2 = a[1] + b[1]
+
+    return (res_1, res_2)
