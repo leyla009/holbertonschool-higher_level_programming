03ab2e20c516db997f2dbe7b2a353c283e52a64b
Leyla <Leyla.khaspolad@gmail.com>
2026-03-29 18:55:50 +0000

    Fix the issues

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-Module 0-add_integer: Provides one function, add_integer(a, b).
-This module is part of the TDD project.
+This module provides a function that adds two integers.
+The function ensures that arguments are either integers or floats.
 """
 
 
 def add_integer(a, b=98):
     """
-    Adds two integers or floats (casted to integers).
+    Adds two integers.
 
     Args:
-        a: First number.
-        b: Second number, defaults to 98.
+        a: The first number (int or float).
+        b: The second number (int or float), defaults to 98.
 
     Returns:
-        The integer addition of a and b.
+        The sum of a and b as an integer.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
