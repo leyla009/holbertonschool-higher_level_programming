d426a456de658a2946f55ea1a5789125a9ce1b14
Leyla <Leyla.khaspolad@gmail.com>
2026-04-12 14:59:26 +0000

    Add task 3

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains the function inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to match against.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
