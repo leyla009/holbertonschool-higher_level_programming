6d00c9fa416f95b441a57ccd0fbebb912e6149e5
Leyla <Leyla.khaspolad@gmail.com>
2026-04-19 13:04:58 +0000

    Add task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module defines a class Student with attribute filtering.
+"""
+
+
+class Student:
+    """
+    Represent a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attributes contained in 
+        this list are retrieved.
+
+        Args:
+            attrs (list): A list of strings representing attribute names.
+
+        Returns:
+            dict: A dictionary containing the filtered student attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            res = {}
+            for k in attrs:
+                if k in self.__dict__:
+                    res[k] = self.__dict__[k]
+            return res
+        return self.__dict__
