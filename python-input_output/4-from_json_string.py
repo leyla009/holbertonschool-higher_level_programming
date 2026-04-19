964b5a9c4c521577da7d25e1a82b8de751fd3c57
Leyla <Leyla.khaspolad@gmail.com>
2026-04-19 12:50:48 +0000

    Add task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module provides a function that converts a JSON string to an object.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to deserialize.
+
+    Returns:
+        any: The Python object representation of the JSON string.
+    """
+    return json.loads(my_str)
