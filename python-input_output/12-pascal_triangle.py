3fcae9d04e3f7ecc8352e7247a1577558a6957ed
Leyla <Leyla.khaspolad@gmail.com>
2026-04-19 13:13:41 +0000

    Fix task 12 issues

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,7 +6,8 @@ This module contains a function that generates Pascal's Triangle.
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal's triangle of n.
+    Returns a list of lists of integers representing
+    the Pascal's triangle of n.
 
     Args:
         n (int): The number of rows of the triangle.
