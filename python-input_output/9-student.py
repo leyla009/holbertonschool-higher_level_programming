4c1c56ba7b75f7e022c5fbf66ce42e80e3d79705
Leyla <Leyla.khaspolad@gmail.com>
2026-04-19 13:03:28 +0000

    Add task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module defines a class Student.
+"""
+
+
+class Student:
+    """
+    Represent a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        return self.__dict__
