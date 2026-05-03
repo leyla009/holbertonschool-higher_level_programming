514b3f69238728bc62939df5220201d866b50718
Leyla <Leyla.khaspolad@gmail.com>
2026-05-03 19:37:53 +0000

    Add task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a State and an instance Base = declarative_base()
+Contains State class and Base, an instance of declarative_base()
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
